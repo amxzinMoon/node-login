e692355c0c0267e64b84eddd7c5099b5f0f73d64
unknown <mgje0115@google.co.kr>
2021-08-24 15:39:00 +0900

    node-login

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+    id: ["11", "문기", "문기기"],
+    psword: ["111", "1234", "1233"],
+};
+
 const output ={
     home: (req, res) => {
         res.render("home/index");
@@ -12,7 +17,22 @@ const output ={
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인 실패.",
+        });
     },
 };
 
