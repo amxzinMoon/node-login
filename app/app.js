4c11b17410393bc05b3abdf660fd2c35039f9a59
unknown <mgje0115@google.co.kr>
2021-08-24 15:20:14 +0900

    dody-parser

--- app/app.js
+++ app/app.js
@@ -11,6 +11,10 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use("/", home); //미들웨어
+
 
 module.exports = app;
