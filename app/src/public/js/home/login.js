f59a078e3402ff711fb605ea580a7368c8c3813d
unknown <mgje0115@google.co.kr>
2021-08-24 15:07:10 +0900

    fetch

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,4 +12,11 @@ function login () {
         psword: psword.value,
     };
 
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 };
